4ebb79fa1e8132744e5734ba512494a163043661
MFR414 <firmansyahm9@gmail.com>
2021-10-03 01:13:52 +0700

    PHP Math function (PI and Min Max)

---
+++ php/math.php
@@ -0,0 +1,19 @@
+<?php
+    echo "PHP pi() Function: <br>";
+    echo "The pi() function returns the value of PI <br><br>";
+    echo "Example:<br>";
+    echo (pi()); // returns 3.1415926535898
+
+    echo "<br><br><br>";
+
+    echo "PHP min() and max() Functions: <br>";
+    echo "The min() and max() functions can be used to find the lowest or highest value in a list of arguments <br><br>";
+    echo "Example:<br>";
+    echo "data:<br>";
+    echo "(0, 150, 30, 300, -10, -250)<br>"; // returns -250
+    echo "min:<br>";
+    echo (min(0, 150, 30, 300, -10, -250)); // returns -250
+    echo "<br>";
+    echo "max:<br>";
+    echo(max(0, 150, 30, 300, -10, -250));  // returns 300
+?>
